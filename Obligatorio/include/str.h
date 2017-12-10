ad70bf4ab1d3dfe9a3d437bc0fec4422546c3ccf
Santiago Mattiauda <santimattius@gmail.com>
2017-12-10 11:21:01 -0300

    Se  agrega class string posta, eliminar cadena

---
+++ Obligatorio/include/str.h
@@ -0,0 +1,31 @@
+#ifndef _Str_H
+#define _Str_H
+
+const int MAX = 80;
+class String
+{
+private:
+    char * cadena;
+public:
+    String();
+    // constructor por defecto.
+    String(char* s);
+    // constructor común
+    String(const String &);
+    // constructor de copia
+    ~String();
+    // destructor
+    String operator=(const String &);
+    // sobrecarga de asignación
+    bool operator==(String);
+    // compara dos strings por igualdad.
+    bool operator< (String);
+    // compara dos strings alfabéticamente
+    String operator+ (String);
+    // concatenación de strings.
+    void scan();
+    // lee un string desde la entrada estándar.
+    void print();
+    // muestra un string en la salida estándar
+};
+#endif
